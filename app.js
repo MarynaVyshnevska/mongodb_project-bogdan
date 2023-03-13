d5cb5ed5f4e412d0ffc28948db81a033de943179
Maryna Vyshnevska <vyshnevska.maryna@gmail.com>
2023-03-13 13:22:12 +0200

    move DB_HOST to config.js and add config.js to .gitignore

--- app.js
+++ app.js
@@ -2,8 +2,7 @@
 // Owu82L1o5XjpXL8W
 const mongoose = require("mongoose");
 
-const DB_HOST =
-  "mongodb+srv://Maryna:Owu82L1o5XjpXL8W@cluster0.nmxzhwe.mongodb.net/books_reader?retryWrites=true&w=majority";
+const { DB_HOST } = require('./config');
 
 mongoose
   .connect(DB_HOST)
